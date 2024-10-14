16c8cbd1c73c260b9e0e3682bc0047a2e93d117c
Iwayemi-Kehinde <iwayemikehinde1@gmail.com>
2024-10-14 04:11:44 -0700

    fixed the bloody bug

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -2,11 +2,11 @@ import { Request, Response, NextFunction } from "express"
 import User from "../models/User.js"
 
 
-export const getAllUsers = async (req:Request, res: Response, next:NextFunction) => {
+export const getAllUsers = async (req:Request, res: Response, next:NextFunction): Promise<any> => {
 	try {
 		const users = await User.find()
 		return res.status(200).json({users})
-	} catch (error) {
+	} catch (error: any) {
 		return res.status(500).json({error: error.message})
 	}
 }
