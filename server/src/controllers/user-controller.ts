7d383819c2aaf8a3ea344375d70a5204266ae1c5
Iwayemi-Kehinde <iwayemikehinde1@gmail.com>
2024-10-17 14:59:37 -0700

    fixed the signup controller bug

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express"
 import User from "../models/User.js"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import { createToken } from "../utils/token-manager.js"
 import { COOKIE_NAME } from "../utils/constants.js"
 
@@ -21,17 +21,16 @@ export const userSignup = async (req: Request, res: Response, next: NextFunction
 		if (existingUser) {
 			return res.status(401).send("User already registered")
 		}
-		const hashedPassword = await bcrypt.hash(password, 50)
+		const saltRound = 10
+		const hashedPassword = await bcrypt.hash(password, saltRound)
 		const user = new User({ name, email, password: hashedPassword })
 		await user.save()
 
 		res.clearCookie(COOKIE_NAME, {httpOnly: true, domain: "localhost", signed: true, path: "/"})
-		const token = createToken(existingUser._id.toString(), existingUser.email, "7d")
+		const token = createToken(user._id.toString(), user.email, "7d")
 		const expires = new Date()
 		expires.setDate(expires.getDate() + 7)
 		res.cookie(COOKIE_NAME, token, { path: "/", domain: "localhost", expires, httpOnly: true, signed: true })
-
-		
 		return res.status(200).json({ message: "OK", user: user._id.toString() })
 	} catch (error: any) {
 		return res.status(500).json({ error: error.message })
